f2cfdd7569e4f4c98a451f856e23451e73e5f423
elijah03ojo <44117114+elijah03ojo@users.noreply.github.com>
2020-01-14 19:43:58 -0500

    Update exercises.js

--- src/exercises.js
+++ src/exercises.js
@@ -1,17 +1,66 @@
 function commonEnd(a, b) {
-  if (!a || a.length === 0 || !b || b.length === 0) {
-    return false;
-  }
-
-  // write your code here
+    if (!a || a.length === 0 || !b || b.length === 0) {
+        return false;
+    }
+    else if (a[0] === b[0]) {
+        return true;
+    }
+    else if (a[a.length - 1] === b[b.length - 1]) {
+        return true;
+    }
+    else {
+        return false;
+    }
 }
 
 function endsMeet(values, n) {
-  // write your code here
+    if (!values || values.length < n || n < 0) {
+        return values = [];
+    }
+    else {
+        let array = [];
+        for (let i = 0; i < n; i++) {
+            array.push(values[i]);
+        }
+        for (let j = values.length - n; j < values.length; j++) {
+            array.push(values[j]);
+        }
+        return array;
+    }
 }
 
 function difference(numbers) {
-  // write your code here
+    let isUndefined = false;
+    let max;
+    let min;
+    if (!numbers || numbers.length < 1) {
+        return undefined;
+        isUndefined = true;
+    }
+    else {
+        for (number of numbers) {
+            if (Number.isNaN(number)) {
+                return undefined;
+                isUndefined = true;
+            }
+        }
+    }
+    if (isUndefined === false) {
+        max = numbers[0];
+        min = numbers[0];
+        for (let i = 1; i < numbers.length; i++) {
+            if (numbers[i] > numbers[i - 1]) {
+                max = numbers[i];
+            }
+        }
+        for (let i = 1; i < numbers.length; i++) {
+            if (numbers[i] < numbers[i - 1]) {
+                min = numbers[i];
+            }
+        }
+        return max - min;
+    }
+
 }
 
 function max(number) {
